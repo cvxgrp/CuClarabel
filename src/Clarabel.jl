fba7121b58b7df3a07de0f0a0cb262bd7229e313
goulart-paul <paul.goulart@eng.ox.ac.uk>
2025-02-07 22:20:48 +0000

    disable HSL

--- src/Clarabel.jl
+++ src/Clarabel.jl
@@ -108,9 +108,9 @@ module Clarabel
         @require Pardiso="46dd5b70-b6fb-5a00-ae2d-e8fea33afaf2" begin
             include("./kktsolvers/direct-ldl/directldl_pardiso.jl")  
         end 
-        @require HSL="34c5aeac-e683-54a6-a0e9-6e0fdc586c50" begin
-            include("./kktsolvers/direct-ldl/directldl_hsl.jl")
-        end 
+        # @require HSL="34c5aeac-e683-54a6-a0e9-6e0fdc586c50" begin
+        #     include("./kktsolvers/direct-ldl/directldl_hsl.jl")
+        # end 
     end
  
     # JSON I/O
